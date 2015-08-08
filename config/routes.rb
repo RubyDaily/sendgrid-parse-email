9bea1208299fa31078b7a6e9df86c68a71d27953
Vinh Nguyen Le <vinhnlx@asiantech.vn>
2015-08-08 23:43:45 +0700

    Set the endpoint for receives the POST messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # Set the endpoint
+  mount_gridder
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
