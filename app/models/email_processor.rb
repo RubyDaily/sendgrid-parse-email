7652d8cc23ee0f8718530823bc80fa3ee3baa3e7
Vinh Nguyen Le <vinhnlx@asiantech.vn>
2015-08-09 00:07:20 +0700

    Implement process method

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,2 +1,12 @@
 class EmailProcessor
+  def initialize(email)
+    @email = email
+  end
+
+  def process
+    title = @email.subject
+    body = @email.body
+
+    Email.create!(title: title, body: body)
+  end
 end
